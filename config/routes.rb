8cb2138e002fc0a911295cc4dc4f801a5de26288
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-09-16 15:49:12 +0300

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :payments
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'static#index'
 end
