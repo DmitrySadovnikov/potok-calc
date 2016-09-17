0134e50fbfd5a564a6ba9e15259405644c7484a8
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-09-17 15:56:53 +0300

    Added new fields to company entity and changed integers to float

--- db/migrate/20160916064401_create_companies.rb
+++ db/migrate/20160916064401_create_companies.rb
@@ -2,11 +2,21 @@ class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :companies do |t|
       t.string :name
-      t.integer :money
+      t.float :money
       t.integer :term
-      t.integer :annual_rate
+      t.float :annual_rate
       t.integer :period
-      t.integer :default_rate
+      t.float :default_rate
+
+      t.float :monthly_debt
+      t.float :monthly_percent
+      t.float :monthly_common
+      t.float :payout
+
+      t.float :paid_percent
+      t.float :paid_debt
+
+      t.float :yield_pa
 
       t.timestamps
     end
--- db/migrate/20160916064503_create_payments.rb
+++ db/migrate/20160916064503_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration[5.0]
   def change
     create_table :payments do |t|
       t.string :name
-      t.integer :amount
+      t.float :amount
       t.references :company, foreign_key: true
 
       t.timestamps
