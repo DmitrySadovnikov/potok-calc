9779dc893fc7bdcf1cb831ccb211c8552ded0f2d
Dmitry Sadovnikov <d.sadovnikov@yahoo.com>
2016-09-18 21:31:08 +0300

    Fix assets for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #for heroku
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
 end
